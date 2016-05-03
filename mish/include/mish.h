1e037bc5dc8094761a183d94758f4f76b3af1611
chris <chris@hichris.com>
2016-05-03 19:01:04 -0400

    Initial commit.

---
+++ mish/include/mish.h
@@ -0,0 +1,15 @@
+/*
+ * mish.h
+ *
+ *  Created on: May 3, 2016
+ *      Author: chris
+ */
+
+#ifndef INCLUDE_MISH_H_
+#define INCLUDE_MISH_H_
+
+#include <string.h>
+
+char mish_parse(String);
+
+#endif /* INCLUDE_MISH_H_ */
