3f28a50f3c91fdb3cfb8962e83ec41014062d84a
Chris Smith <chris@hichris.com>
2016-09-20 10:31:37 -0400

    Added support for multithreading with the execution.

--- include/executer.h
+++ include/executer.h
@@ -9,7 +9,28 @@
 #define INCLUDE_EXECUTER_H_
 
 #include <mish.h>
+#include <stack.h>
 
 void mish_execute(Code* code);
+
+enum ExecuteMode {
+	BYTECODE_MODE, ARGUMENT_MODE
+};
+
+class ExecuterState {
+public:
+	ExecuterState();
+	~ExecuterState();
+
+	Stack<ExecuteMode>* modeStack;
+
+	Stack<Iterator<Bytecode*>*>* callStack;
+
+	Stack<Iterator<Expression*>*>* argumentIteratorStack;
+	Stack<List<Value*>*>* evaluationsStack;
+
+	Stack<Function*>* functionStack;
+	Value* returnValue;
+};
 
 #endif /* INCLUDE_EXECUTER_H_ */
--- src/executer.cpp
+++ src/executer.cpp
@@ -7,101 +7,154 @@
 
 #include <executer.h>
 
-enum ExecuteMode {
-	BYTECODE_MODE, ARGUMENT_MODE
+ExecuterState::ExecuterState() {
+	modeStack = new Stack<ExecuteMode>();
+
+	callStack = new Stack<Iterator<Bytecode*>*>();
+
+	argumentIteratorStack = new Stack<Iterator<Expression*>*>();
+	evaluationsStack = new Stack<List<Value*>*>();
+
+	functionStack = new Stack<Function*>();
+	returnValue = NULL;
+}
+
+ExecuterState::~ExecuterState() {
+	delete modeStack;
+
+	while (callStack->size() > 0) {
+		delete callStack->pop();
+	}
+	delete callStack;
+
+	while (argumentIteratorStack->size() > 0) {
+		delete argumentIteratorStack->pop();
+	}
+	delete argumentIteratorStack;
+
+	while (evaluationsStack->size() > 0) {
+		List<Value*>* evaluations = evaluationsStack->pop();
+		Iterator<Value*> evaluationIterator = evaluations->iterator();
+		while (evaluationIterator.hasNext()) {
+			Value* value = evaluationIterator.next();
+			if (!value->isConstant) {
+				delete value;
+			}
+		}
+		delete evaluations;
+	}
+	delete evaluationsStack;
+
+	delete functionStack;
+
+	if (returnValue != NULL) {
+		delete returnValue;
+	}
+}
+
+enum ExecuteStatus {
+	DONE, NOT_DONE
 };
 
+ExecuteStatus execute(ExecuterState* state) {
+	if (state->modeStack->peek() == BYTECODE_MODE) {
+		if (state->callStack->peek()->hasNext()) {
+			Bytecode* bytecode = state->callStack->peek()->next();
+			switch (bytecode->instruction) {
+			case FUNC_CALL:
+				FunctionCallVoid* functionCallVoid =
+						(FunctionCallVoid*) bytecode;
+
+				state->argumentIteratorStack->push(
+						new Iterator<Expression*>(
+								functionCallVoid->arguments->iterator()));
+				state->evaluationsStack->push(new List<Value*>());
+				state->functionStack->push(functionCallVoid->function);
+				state->modeStack->push(ARGUMENT_MODE);
+
+				break;
+			}
+		} else {
+			// TODO end of function... if non-void function, crash system, if void function, then return
+			// for now, just end the execution
+			delete state->callStack->pop();
+			state->modeStack->pop();
+			return DONE;
+		}
+	} else if (state->modeStack->peek() == ARGUMENT_MODE) {
+		if (state->argumentIteratorStack->peek()->hasNext()) {
+			Expression* expression =
+					state->argumentIteratorStack->peek()->next();
+			switch (expression->expressionType) {
+			case VALUE_EXPRESSION: {
+				Value* constant = (Value*) expression;
+				constant->isConstant = true;
+				state->evaluationsStack->peek()->add(constant);
+			}
+				break;
+			case FUNCTION_EXPRESSION:
+				FunctionCallReturn* functionCallReturn =
+						(FunctionCallReturn*) expression;
+
+				state->argumentIteratorStack->push(
+						new Iterator<Expression*>(
+								functionCallReturn->arguments->iterator()));
+				state->evaluationsStack->push(new List<Value*>());
+				state->functionStack->push(functionCallReturn->function);
+				state->modeStack->push(ARGUMENT_MODE);
+
+				break;
+			}
+		} else {
+			// out of evaluations, call the function
+			delete state->argumentIteratorStack->pop();
+			List<Value*>* evaluations = state->evaluationsStack->pop();
+			Function* function = state->functionStack->pop();
+
+			if (function->native != NULL) {
+				state->returnValue = function->native(evaluations);
+				// TODO delete the return value at some point
+				state->modeStack->pop();
+				if (state->modeStack->peek() == ARGUMENT_MODE) {
+					state->evaluationsStack->peek()->add(state->returnValue);
+				}
+			} else {
+				// TODO push callStack
+				crash(L"non-native function calls not implemented yet");
+			}
+
+			Iterator<Value*> evaluationIterator = evaluations->iterator();
+			while (evaluationIterator.hasNext()) {
+				Value* value = evaluationIterator.next();
+				if (!value->isConstant) {
+					delete value;
+				}
+			}
+			delete evaluations;
+		}
+	} else {
+		// TODO fault as error in Mish and continue
+		crash(L"unknown execution mode");
+	}
+
+	return NOT_DONE;
+}
+
 void mish_execute(Code* code) {
-	// stacks
-	Stack<Iterator<Bytecode*>*> callStack;
-	Stack<Iterator<Expression*>*> argumentIteratorStack;
-	Stack<List<Value*>*> evaluationsStack;
-	Stack<Function*> functionStack;
-	Stack<ExecuteMode> modeStack;
-	Value* returnValue;
+	ExecuterState* state = new ExecuterState();
 
 	// start executing on first bytecode
 	// TODO change this so that execution can resume and exit after every cycle
-	callStack.push(new Iterator<Bytecode*>(code->bytecodes->iterator()));
-	modeStack.push(BYTECODE_MODE);
+	state->callStack->push(
+			new Iterator<Bytecode*>(code->bytecodes->iterator()));
+	state->modeStack->push(BYTECODE_MODE);
 
 	while (true) {
-		if (modeStack.peek() == BYTECODE_MODE) {
-			if (callStack.peek()->hasNext()) {
-				Bytecode* bytecode = callStack.peek()->next();
-				switch (bytecode->instruction) {
-				case FUNC_CALL:
-					FunctionCallVoid* functionCallVoid =
-							(FunctionCallVoid*) bytecode;
-
-					argumentIteratorStack.push(
-							new Iterator<Expression*>(
-									functionCallVoid->arguments->iterator()));
-					evaluationsStack.push(new List<Value*>());
-					functionStack.push(functionCallVoid->function);
-					modeStack.push(ARGUMENT_MODE);
-
-					break;
-				}
-			} else {
-				// TODO end of function... if non-void function, crash system, if void function, then return
-				// for now, just end the execution
-				delete callStack.pop();
-				modeStack.pop();
-				break;
-			}
-		} else if (modeStack.peek() == ARGUMENT_MODE) {
-			if (argumentIteratorStack.peek()->hasNext()) {
-				Expression* expression = argumentIteratorStack.peek()->next();
-				switch (expression->expressionType) {
-				case VALUE_EXPRESSION: {
-					Value* constant = (Value*) expression;
-					constant->isConstant = true;
-					evaluationsStack.peek()->add(constant);
-				}
-					break;
-				case FUNCTION_EXPRESSION:
-					FunctionCallReturn* functionCallReturn =
-							(FunctionCallReturn*) expression;
-
-					argumentIteratorStack.push(
-							new Iterator<Expression*>(
-									functionCallReturn->arguments->iterator()));
-					evaluationsStack.push(new List<Value*>());
-					functionStack.push(functionCallReturn->function);
-					modeStack.push(ARGUMENT_MODE);
-
-					break;
-				}
-			} else {
-				// out of evaluations, call the function
-				delete argumentIteratorStack.pop();
-				List<Value*>* evaluations = evaluationsStack.pop();
-				Function* function = functionStack.pop();
-
-				if (function->native != NULL) {
-					returnValue = function->native(evaluations);
-					// TODO delete the return value at some point
-					modeStack.pop();
-					if (modeStack.peek() == ARGUMENT_MODE) {
-						evaluationsStack.peek()->add(returnValue);
-					}
-				} else {
-					// TODO push callStack
-					crash(L"non-native function calls not implemented yet");
-				}
-
-				Iterator<Value*> evaluationIterator = evaluations->iterator();
-				while (evaluationIterator.hasNext()) {
-					Value* value = evaluationIterator.next();
-					if (!value->isConstant) {
-						delete value;
-					}
-				}
-				delete evaluations;
-			}
-		} else {
-			crash(L"unknown execution mode");
+		ExecuteStatus status = execute(state);
+		if (status == DONE) {
+			break;
 		}
 	}
+
+	delete state;
 }
